ec57af8586d1acb13fba6f811684906b2e0004f5
Freek <freek@spatie.be>
2018-01-10 14:30:18 +0100

    add invalid link

--- tests/server/resources/views/home.blade.php
+++ tests/server/resources/views/home.blade.php
@@ -4,3 +4,4 @@
 <a href="/noResponse">This url does not give a response</a>
 <a href="/linkRelProfile">This url has a link rel node</a>
 <a href="/linkRelStyleSheet">This url has a link stylesheet node</a>
+<a href="https:///invalid.domain/broken">This url has a link stylesheet node</a>
